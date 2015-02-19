1f9915cadd7d4887981b8dc13c986e0732e96bec
JimHallett <jhallett@ewu.edu>
2015-02-19 14:49:44 -0800

    final table 1

--- Rscripts/SamplingLocations.R
+++ Rscripts/SamplingLocations.R
@@ -33,7 +33,7 @@ UTMsampleYear <- UTM %>%
   select(-Station, -Year1:-Yr3, -Easting) %>%
   arrange(Unit, Habitat) %>%
   group_by(Owner, Unit, Habitat, YearsSampled) %>%
-  count(Unit, Habitat)
+  count(Owner, Unit, Habitat, YearsSampled)
 
 
 
@@ -46,14 +46,33 @@ options('ReporteRs-fontsize'=11, 'ReporteRs-default-font'='Times New Roman')
 
 reportout = docx(template = "Annual Progress Report 2014.docx") 
 
-#Table1 <- vanilla.table(UTMSampleYear,   add.rownames = FALSE)
-#Table1 <-   FlexTable(UTMsampleYear)
-Table1 <-   FlexTable(UTMsampleYear)
-Table1[, 1:6] <-   parProperties(text.align = "left")
+
+
+Table1 <-   FlexTable(UTMsampleYear, header.columns = FALSE)
+
+Table1 = addHeaderRow(Table1, 
+                      value = c( "Owner", "Management unit", "Habitat", "Years sampled", "Number of sampling points"),
+                     cell.properties = cellProperties(padding.bottom = 3, padding.top = 3))
+
+
+setFlexTableBorders(Table1,inner.vertical=borderProperties(style = "none"),
+                    inner.horizontal=borderProperties(style = "none"),
+                    outer.vertical=borderProperties(style = "none"),
+                    outer.horizontal=borderProperties(),
+                    body=TRUE,header=TRUE,footer=FALSE)
+spanFlexTableRows(Table1, j = "Owner", runs = as.character( UTMsampleYear$Owner ) )
+
+Table1[, 1:4] <-   parProperties(text.align = "left")
+Table1[, 5] <-   parProperties(text.align = "center")
+
+
+
+
+#Table1 <- borderNone()
 Table1 <-   setZebraStyle(Table1, odd = '#eeeeee', even = 'white' ) 
 
 reportout = addFlexTable(reportout, Table1, bookmark = "Table1") 
-  writeDoc( reportout, file = "Report2015.docx")
+  writeDoc( reportout, file = "Report2015.docx", par.properties = parProperties(text.align = "center"),)
 
 # 
 # reportout = docx(template = "Annual Progress Report 2014.docx") %>%
